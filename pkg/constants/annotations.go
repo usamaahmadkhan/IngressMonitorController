50464e8bccafaad8fad02b1bfccbe8497a441612
Jess Belliveau <jbelliveau@apptio.com>
2019-03-21 14:53:35 +1100

    Allow annotation to override host

--- pkg/constants/annotations.go
+++ pkg/constants/annotations.go
@@ -3,6 +3,7 @@ package constants
 const (
 	ForceHTTPSAnnotation     = "monitor.stakater.com/forceHttps"
 	OverridePathAnnotation   = "monitor.stakater.com/overridePath"
+	OverrideHostAnnotation   = "monitor.stakater.com/overrideHost"
 	MonitorEnabledAnnotation = "monitor.stakater.com/enabled"
 	MonitorNameAnnotation    = "monitor.stakater.com/name"
 	MonitorHealthAnnotation  = "monitor.stakater.com/healthEndpoint" // "/health"
